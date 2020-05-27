fbc8dbb4da8f98d5a5f781777508d4e2c8f3a550
systemthinker <robbertoutenaar@gmail.com>
2020-05-27 12:34:33 +0200

    user and image models and migrations

---
+++ migrations/20200527102539-create-user.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      fullName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
---
+++ models/image.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const image = sequelize.define(
+    'image',
+  {
+    title: DataTypes.STRING,
+    url: DataTypes.STRING,
+  }, {});
+  image.associate = function(models) {
+    // associations can be defined here
+  };
+  return image;
+};
